a5d1f40a88d6ebc303e57f9da610f11705f66385
Christian Obinna Iheacho <christianobinna.iheacho@student.griffith.ie>
2022-02-10 14:11:27 +0000

    css modifications

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,15 +8,15 @@ const Menu = ({ menuItems }) => {
         <h2 className="our-menu">Our Menu</h2>
         <NavBar />
         {menuItems.map((menus) => {
-          const { id, title, category, price, img, desc } = menus;
+          const { id, title, price, img, desc } = menus;
           return (
             <>
-              <div className="col-md-6" style={{ marginTop: "30px" }}>
+              <div className="col-md-6" key={id} style={{ marginTop: "30px" }}>
                 <div className="row">
-                  <div className="col-md-4 imgx">
-                    <img src={img} alt="name" height={170} width={200} />
+                  <div className="col-md-5 imgx">
+                    <img src={img} alt="name" className="photos" />
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-md-7">
                     <h5>
                       {title}
                       <p style={{ float: "right", color: "#e3b20b" }}>
